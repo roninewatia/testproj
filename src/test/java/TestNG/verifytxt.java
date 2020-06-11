ee45c4a8eb381159cc6fce5c904a5466629c661e
root <root@ip-172-31-29-144.us-east-2.compute.internal>
2020-06-11 16:57:54 +0000

    fixed compile error

--- src/test/java/TestNG/verifytxt.java
+++ src/test/java/TestNG/verifytxt.java
@@ -4,6 +4,7 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
